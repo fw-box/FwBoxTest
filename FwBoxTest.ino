b3b34519e7dfe7ecb204740d45e9c2507dc704af
fw-box <77254167+fw-box@users.noreply.github.com>
2022-01-23 15:51:54 +0800

    Add files via upload

---
+++ FwBoxTest.ino
@@ -0,0 +1,28 @@
+//
+// Copyright (c) 2022 Fw-Box (https://fw-box.com)
+// Author: Hartman Hsieh
+//
+// Description :
+//   None
+//
+// Connections :
+//   None
+//
+// Required Library :
+//   None
+//
+
+#define FIRMWARE_VERSION "1.0.0"
+
+void setup()
+{
+  Serial.begin(115200);
+
+  Serial.println("begin...");
+} // void setup()
+
+void loop()
+{
+  Serial.printf("millis() = %d\n", millis());
+  delay(3000);
+} // END OF "void loop()"
